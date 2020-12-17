4b0e3c3220865b0d1cc23d52df54e7587b561e61
Evgenij00 <49844107+Evgenij00@users.noreply.github.com>
2020-12-17 15:09:04 +0300

    Добавьте файлы проекта.

---
+++ courseProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace courseProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
