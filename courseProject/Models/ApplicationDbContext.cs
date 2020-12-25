4833a6c2c28884d1707c02201e8e65bcad5c0247
Denis Pakhomov <denis.pakhomov21@yandex.ru>
2020-12-25 14:50:37 +0300

    Замокал данные (SampleData.cs). Объвил в ApplicationDbContext Categories и Tests

--- courseProject/Models/ApplicationDbContext.cs
+++ courseProject/Models/ApplicationDbContext.cs
@@ -8,6 +8,9 @@ namespace myCourseProject.Models
 {
     public class ApplicationDbContext : DbContext //Чтобы создать контекст, нам надо унаследовать новый класс от класса DbContext (AplicationContext)
     {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Test> Tests { get; set; }
+
         //По умолчанию у нас база данных отсутствуют.Поэтому в конструктор TestsContext определен вызов Database.EnsureCreated(),
         //который при отсутствии базы данных автоматически создает ее.Если база данных уже есть, то ничего не происходит.
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) //Конструктор
