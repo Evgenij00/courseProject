fe60068e46091dbaf3b3f70a69fb74f5a95e7f76
Denis Pakhomov <denis.pakhomov21@yandex.ru>
2020-12-25 03:14:23 +0300

    Добавил модели Test

---
+++ courseProject/Models/Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myCourseProject.Models
+{
+    public class Test
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Icon { get; set; }
+        public string Description { get; set; }
+        public string Autors { get; set; }
+        public int NumberOfQuestions { get; set; }
+
+        public int CategoryId { get; set; }
+    }
+}
