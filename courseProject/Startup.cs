d06c36fda342512ace0d556922e3d322d0729503
Evgenij00 <49844107+Evgenij00@users.noreply.github.com>
2020-12-26 14:57:19 +0300

    Изменил Startup.cs

--- courseProject/Startup.cs
+++ courseProject/Startup.cs
@@ -4,10 +4,13 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using courseProject.Models;
+using Microsoft.EntityFrameworkCore; 
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 
 namespace courseProject
 {
@@ -17,12 +20,12 @@ namespace courseProject
         {
             Configuration = configuration;
         }
-
         public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string connection = Configuration.GetConnectionString("DefaultConnection");   //???
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connection)); //???
             services.AddControllersWithViews();
         }
 
@@ -39,11 +42,11 @@ namespace courseProject
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
-
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
