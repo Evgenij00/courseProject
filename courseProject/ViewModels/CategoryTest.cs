3da5e7b9a0cf43b0505ab89eb874f0d11a874ff5
Denis Pakhomov <denis.pakhomov21@yandex.ru>
2020-12-26 16:26:09 +0300

    Добавил ViewModel для главной страницы

---
+++ courseProject/ViewModels/CategoryTest.cs
@@ -0,0 +1,14 @@
+﻿using courseProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace courseProject.ViewModels
+{
+    public class CategoryTest
+    {
+        public IEnumerable<Test> Tests { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+    }
+}
