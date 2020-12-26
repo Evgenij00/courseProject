9572bae89bd29c81ccb6f3b5ac1456c6eb7eb574
Denis Pakhomov <denis.pakhomov21@yandex.ru>
2020-12-26 16:19:30 +0300

    Добавил главный контроллер для мастер-страницы

---
+++ courseProject/Controllers/ApplicationController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using courseProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace courseProject.Controllers
+{
+    public abstract class ApplicationController : Controller
+    {
+
+        private ApplicationDbContext _db; // объявление переменной, в которой будет находиться контекст данных
+
+        public ApplicationDbContext DataContext // геттер для получения контекста данных из внешнего кода
+        {
+            get { return _db; }
+        }
+        public ApplicationController(ApplicationDbContext context) //конструктор, в котором получаем контекст данных
+        {
+            _db = context;
+        }
+    }
+}
