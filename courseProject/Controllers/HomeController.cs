553167148568671538368ddac8716a048b29747b
Denis Pakhomov <denis.pakhomov21@yandex.ru>
2020-12-26 16:15:39 +0300

    Переопределил метод Index() HomeController

--- courseProject/Controllers/HomeController.cs
+++ courseProject/Controllers/HomeController.cs
@@ -1,37 +1,51 @@
 ﻿using courseProject.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using courseProject.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 
 namespace courseProject.Controllers
 {
-    public class HomeController : Controller
+    //TODO: разобрать как инкапсулировать общие действия в ApplicationController
+    public class HomeController : ApplicationController
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
+        //private TestsContext db;
+        public HomeController(ApplicationDbContext context) : base(context)
+        { //ничего не выполняет. Вызывает конструктор ApplicationController
+            //List<Category> temp = DataContext.Categories.ToList();
+            //ViewBag.Categories = temp;
         }
 
+        //Каждый метод контроллера по умоланию использует одноименное представление.
+        //То есть метод Index будет использовать представление Index.cshtml.
         public IActionResult Index()
         {
-            return View();
-        }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+            //И в этот метод передаются все объекты из таблицы Tests в базе данных.
+            //Для передачи данных нам достаточно использовать такую конструкцию: db.Tests.ToList().
+            //return View(db.Tests.ToList());
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            //TODO: нужно инкапсулировать в ApplicationController
+            //ViewBag.Categories = DataContext.Categories.OrderBy(c => c.Id);
+
+            ViewBag.Categories = DataContext.Categories;
+
+            //TODO: Решить, создавать контейнер для нескольких моделей или все передавать через ViewBag
+            CategoryTest model = new CategoryTest();
+
+            //TODO: разобраться, необходим ли метод ToList()
+            model.Tests = DataContext.Tests.OrderBy(t => t.Id).ToList();
+            model.Categories = DataContext.Categories.OrderBy(c => c.Id).ToList();
+
+            return View(model);
+            //TODO: Решить, создавать контейнер для нескольких моделей или все передавать через ViewBag
         }
     }
 }
+//Во-первых, здесь удалены все ненужные нам методы - все кроме метода Index,
+//который будет использоваться для передачи пользователю данных о товарах.
