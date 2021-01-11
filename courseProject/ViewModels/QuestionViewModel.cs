68dd472250d2a2edd3e2324eb545d5f3bea76650
Evgenij00 <49844107+Evgenij00@users.noreply.github.com>
2021-01-12 00:18:18 +0300

    создал QuestionViewModel

---
+++ courseProject/ViewModels/QuestionViewModel.cs
@@ -0,0 +1,14 @@
+﻿using courseProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myCourseProject.ViewModels
+{
+    public class QuestionViewModel
+    {
+        public Test Test { get; set; }
+        public Question Question { get; set; }
+    }
+}
