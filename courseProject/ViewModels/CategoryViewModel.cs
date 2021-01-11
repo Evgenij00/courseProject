8bc472e9ac2042e59dfad198ee7aef9414671c31
Varvara Rakova <vvrak110@mail.ru>
2021-01-11 18:49:10 +0300

    Добавила CategoryViewModel

---
+++ courseProject/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CourseProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseProject.ViewModels
+{
+    public class CategoryViewModel
+    {
+        public Category Category { get; set; }
+        public IEnumerable<Test> Tests { get; set; }
+    }
+}
