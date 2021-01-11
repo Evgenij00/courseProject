7ba3cb4f12e397e5f9100a7d3832d564161b3b1b
Varvara Rakova <vvrak110@mail.ru>
2021-01-11 19:01:42 +0300

    Исправила ошибку в названии файла

--- courseProject/ViewModels/CategoryViewModel.cs
+++ courseProject/ViewModels/CategoryViewModel.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CourseProject.ViewModels
+namespace courseProject.ViewModels
 {
     public class CategoryViewModel
     {
