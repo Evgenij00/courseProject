b064d38eec1564d9a0dc944eaa438fd6f76a70fb
Denis Pakhomov <denis.pakhomov21@yandex.ru>
2021-01-13 01:07:28 +0300

    Добавил User в контекст данных

--- courseProject/Models/ApplicationDbContext.cs
+++ courseProject/Models/ApplicationDbContext.cs
@@ -6,20 +6,26 @@ using System.Threading.Tasks;
 
 namespace courseProject.Models
 {
-    public class ApplicationDbContext : DbContext //Чтобы создать контекст, нам надо унаследовать новый класс от класса DbContext (AplicationContext)
+    //Чтобы создать контекст, нам надо унаследовать новый класс от класса DbContext
+    public class ApplicationDbContext : DbContext
     {
+        //Свойства наподобие public DbSet<Phone> Phones { get; set; }
+        //помогают получать из БД набор данных определенного типа(например, набор объектов User). 
+        //Фактически каждое свойство DbSet будет соотноситься с отдельной таблицей в базе данных.
+
+        public DbSet<User> Users { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Test> Tests { get; set; }
         public DbSet<Question> Questions { get; set; }
         public DbSet<Answer> Answers { get; set; }
         public DbSet<ResultTest> ResultTests { get; set; }
 
-        //По умолчанию у нас база данных отсутствуют.Поэтому в конструктор TestsContext определен вызов Database.EnsureCreated(),
-        //который при отсутствии базы данных автоматически создает ее.Если база данных уже есть, то ничего не происходит.
+        
+        //По умолчанию у нас база данных отсутствует. Поэтому в конструктор ApplicationDbContext определен вызов Database.EnsureCreated(),
+        //который при отсутствии базы данных автоматически создает ее. Если база данных уже есть, то ничего не происходит.
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) //Конструктор
         {
             Database.EnsureCreated();
         }
-
     }
 }
