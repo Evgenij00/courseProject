7b2a7854aff18a359d904c14227c0dcee258616b
Varvara Rakova <vvrak110@mail.ru>
2021-01-12 17:50:07 +0300

    Добавила ResultViewModel в модель-представление

---
+++ courseProject/ViewModels/ResultViewModel.cs
@@ -0,0 +1,15 @@
+﻿using courseProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace courseProject.ViewModels
+{
+    public class ResultViewModel
+    {
+        public Test Test { get; set; }
+        public Category Category { get; set; }
+        public ResultTest ResultTest { get; set; }
+    }
+}
