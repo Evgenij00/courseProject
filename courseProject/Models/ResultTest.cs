2213acb07a41bd183aa50d389387d69e0294b964
Evgenij00 <49844107+Evgenij00@users.noreply.github.com>
2021-01-12 17:20:18 +0300

    подкорректировал данные модели

--- courseProject/Models/ResultTest.cs
+++ courseProject/Models/ResultTest.cs
@@ -8,8 +8,7 @@ namespace courseProject.Models
     public class ResultTest
     {
         public int Id { get; set; }
-        //TODO: Description вместо Descriptions
-        public string Descriptions { get; set; }
+        public string Description { get; set; }
         public int Scores { get; set; }
         public int TestId { get; set; }
         public Test Test { get; set; }
