ca5f68176a55f6686f2e3d3bd430fe9751375562
Denis Pakhomov <denis.pakhomov21@yandex.ru>
2021-01-13 01:04:51 +0300

    Добавил модель User

---
+++ courseProject/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace courseProject.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Avatar { get; set; }
+        public int Gender { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime Birthday { get; set; }
+    }
+}
