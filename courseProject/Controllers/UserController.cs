2616e2e5054829bd4a8495217433989259dd21c9
Denis Pakhomov <denis.pakhomov21@yandex.ru>
2021-01-14 20:58:16 +0300

    Добавил метод ChangePersonalData и ChangeRegisterData

--- courseProject/Controllers/UserController.cs
+++ courseProject/Controllers/UserController.cs
@@ -6,6 +6,8 @@ using courseProject.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using System.Linq;
+using сourseProject.ViewModels;
+using System.Collections.Generic;
 
 namespace courseProject.Controllers
 {
@@ -25,6 +27,77 @@ namespace courseProject.Controllers
             }
 
             return View(currentUser);
+        }
+
+        //TODO: валидация модели
+        public async Task<IActionResult> ChangePersonalData(PersonalViewModel model)
+        {
+            //if (ModelState.IsValid) {}
+            //return View(model);
+
+            string userName = User.Identity.Name;
+            User currentUser = DataContext.Users.FirstOrDefault(u => u.Email == userName);
+
+            if (currentUser != null)
+            {
+                // обновляем пользователя в бд
+                currentUser.FirstName = model.FirstName;
+                currentUser.LastName = model.LastName;
+                currentUser.Gender = model.Gender;
+                currentUser.Birthday = model.Birthday;
+                await DataContext.SaveChangesAsync();
+            }
+
+            return RedirectToAction("Index", "User");
+        }
+
+        //TODO: одинаковый email
+        public async Task<IActionResult> ChangeRegisterData(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+                User user = await DataContext.Users.FirstOrDefaultAsync(u => u.Email == model.Email);
+
+                if (user == null)
+                {
+
+                    string userName = User.Identity.Name;
+
+                    User currentUser = DataContext.Users.FirstOrDefault(u => u.Email == userName);
+
+                    if (currentUser != null)
+                    {
+                        // обновляем пользователя в бд
+                        currentUser.Email = model.Email;
+                        currentUser.Password = model.Password;
+
+                        await DataContext.SaveChangesAsync();
+
+                        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+                        //Аутентификация
+                        List<Claim> claims = new List<Claim>
+                            {
+                                new Claim(ClaimsIdentity.DefaultNameClaimType, model.Email)
+                            };
+
+                        ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+
+                        await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
+                        //Аутентификация
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Некорректные логин и(или) пароль");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Пользователь с таким Email уже существует");
+                }
+            }
+            return RedirectToAction("Index", "User");
         }
     }
 }
