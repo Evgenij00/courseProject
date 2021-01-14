afe120462e7e13cf7734c2fb9774f5be732536ff
Denis Pakhomov <denis.pakhomov21@yandex.ru>
2021-01-14 19:58:07 +0300

    Добавил контроллер изменения данных пользователя

---
+++ courseProject/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Claims;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using courseProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace courseProject.Controllers
+{
+    public class UserController : ApplicationController
+    {
+        public UserController(ApplicationDbContext context) : base(context) { }
+
+        public async Task<IActionResult> IndexAsync()
+        {
+            string userName = User.Identity.Name;
+
+            User currentUser = DataContext.Users.FirstOrDefault(u => u.Email == userName);
+
+            if (currentUser == null)
+            {
+                return NotFound();
+            }
+
+            return View(currentUser);
+        }
+    }
+}
