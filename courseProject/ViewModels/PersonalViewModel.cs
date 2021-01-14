1f2cbf35aa6b403ff037f82ffcec5d10950b8e54
Denis Pakhomov <denis.pakhomov21@yandex.ru>
2021-01-14 19:55:35 +0300

    Добавил модель-представление для изменения персональных данных пользователя

---
+++ courseProject/ViewModels/PersonalViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace courseProject.ViewModels
+{
+    public class PersonalViewModel
+    {
+        [Required]
+        [Display(Name = "FirstName")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "LastName")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Gender")]
+        public int Gender { get; set; }
+
+        [Required]
+        [Display(Name = "Birthday")]
+        public DateTime Birthday { get; set; }
+    }
+}
