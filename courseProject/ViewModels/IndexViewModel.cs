3df04e62591b17e5c436d261b998b55b4b32af81
varvararakova <vvrak110@mail.ru>
2021-01-17 22:56:10 +0300

    Добавила модель-представление для главной страницы

---
+++ courseProject/ViewModels/IndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿using courseProject.Models;
+using System.Collections.Generic;
+
+namespace courseProject.ViewModels
+{
+    public class IndexViewModel
+    {
+        public List<Test> Tests { get; set; }
+        public PageViewModel PageViewModel { get; set; }
+    }
+}
